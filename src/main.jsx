c261e93855e68ae5d19c4dfd4ae99bd14bb86490
T31K <t31kmunwong@gmail.com>
2023-11-16 23:50:27 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
-const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-
+const clerkPubKey = process.env.CLERK_PUBLISHABLE_KEY;
+console.log(clerkPubKey);
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
